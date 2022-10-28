26adc8a9601cc7b93b5226b1684f72684948f765
Oom Lout <aaron@oomlout.com>
2022-10-28 16:52:47 +0100

    Raw parts test.

--- PROJ/ADAF/1755/STAN/01/detailspartsRaw.py
+++ PROJ/ADAF/1755/STAN/01/detailspartsRaw.py
@@ -1,2 +1,2 @@
 def load(newPart,it):
-    it['PROJ-ADAF-1755-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'LED1', 'Value': '', 'Device': 'LEDSMT1206', 'Package': '1206', 'Description': 'LED', 'BOM': ''}, {'Part': 'R1', 'Value': '', 'Device': 'R-US_R0603', 'Package': 'R0603', 'Description': 'RESISTOR, American symbol', 'BOM': ''}]}, {}, {}, {}, {}, {}, {}]
+    it['PROJ-ADAF-1755-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'LED1', 'Value': '', 'Device': 'LEDSMT1206', 'Package': '1206', 'Description': 'LED', 'BOM': ''}, {'Part': 'R1', 'Value': '', 'Device': 'R-US_R0603', 'Package': 'R0603', 'Description': 'RESISTOR, American symbol', 'BOM': ''}]}, {}, {}, {}, {}, {}, {}, {}]
