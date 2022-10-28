ebb5e6cb19055bbd0d50a39bba126fb17a486164
Oom Lout <aaron@oomlout.com>
2022-10-28 14:55:56 +0100

    Changed default voltage on CAPC-0805-X-UF10

--- PROJ/ADAF/2566/STAN/01/detailspartsRaw.py
+++ PROJ/ADAF/2566/STAN/01/detailspartsRaw.py
@@ -1,2 +1,2 @@
 def load(newPart,it):
-    it['PROJ-ADAF-2566-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'CN2', 'Value': '', 'Device': 'JST_3PIN', 'Package': 'JSTPH3', 'Description': '', 'BOM': ''}, {'Part': 'U$2', 'Value': 'FIDUCIAL', 'Device': 'FIDUCIAL', 'Package': 'FIDUCIAL_1MM', 'Description': 'For use by pick and place machines to calibrate the vision/machine, 1mm', 'BOM': ''}]}, {}]
+    it['PROJ-ADAF-2566-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'CN2', 'Value': '', 'Device': 'JST_3PIN', 'Package': 'JSTPH3', 'Description': '', 'BOM': ''}, {'Part': 'U$2', 'Value': 'FIDUCIAL', 'Device': 'FIDUCIAL', 'Package': 'FIDUCIAL_1MM', 'Description': 'For use by pick and place machines to calibrate the vision/machine, 1mm', 'BOM': ''}]}, {}, {}, {}]
--- PROJ/SPAR/8619/STAN/01/detailspartsRaw.py
+++ PROJ/SPAR/8619/STAN/01/detailspartsRaw.py
@@ -1,2 +1,2 @@
 def load(newPart,it):
-    it['PROJ-SPAR-8619-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': []}, {}]
+    it['PROJ-SPAR-8619-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': []}, {}, {}, {}]
