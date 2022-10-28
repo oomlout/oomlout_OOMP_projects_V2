fbd3f624ec621abb55d900deef17617dd59f4274
Oom Lout <aaron@oomlout.com>
2022-10-28 18:23:24 +0100

    Fixed changing detailspartysRaws.

--- PROJ/ADAF/1135/STAN/01/detailspartsRaw.py
+++ PROJ/ADAF/1135/STAN/01/detailspartsRaw.py
@@ -1,2 +1,2 @@
 def load(newPart,it):
-    it['PROJ-ADAF-1135-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'JP1', 'Value': '', 'Device': 'RASBERRYPI_IDC', 'Package': 'RASBERRYPI_IDC', 'Description': 'Rasberry PI IDC Header', 'BOM': ''}]}, {}, {}, {}, {}, {}, {}, {}]
+    it['PROJ-ADAF-1135-STAN-01']['rawParts'] = [{'kicadBom': [], 'eagleBom': [{'Part': 'JP1', 'Value': '', 'Device': 'RASBERRYPI_IDC', 'Package': 'RASBERRYPI_IDC', 'Description': 'Rasberry PI IDC Header', 'BOM': ''}]}]
