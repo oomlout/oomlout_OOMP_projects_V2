a094eeab25e98ac9a528cf5c6a2e4022d8a36cb3
Oom Lout <aaron@oomlout.com>
2022-11-03 22:53:58 +0000

    Full parts oomp added.

--- PROJ/ADAF/1135/STAN/01/detailsPartsOomp.py
+++ PROJ/ADAF/1135/STAN/01/detailsPartsOomp.py
@@ -1,2 +1,2 @@
 def load(newPart,it):
-    it['PROJ-ADAF-1135-STAN-01']['oompParts'] = [{'JP1': 'HEAD-I01-X-UNMATCHED-01'}]
+    it['PROJ-ADAF-1135-STAN-01']['oompParts'] = [{'JP1': {'OOMPID': 'HEAD-I01-X-UNMATCHED-01', 'FULL': {'PART': 'JP1', 'PARTLETTER': 'JP', 'VALUE': '', 'VALUENUMBER': '', 'DEVICE': 'RASBERRYPI_IDC', 'PACKAGE': 'RASBERRYPI_IDC', 'PACKAGENUMBER': '', 'DESC': 'Rasberry PI IDC Header', 'BOM': '', 'OWNER': 'ADAF', 'FULL': 'JP1,,RASBERRYPI_IDC,RASBERRYPI_IDC,Rasberry PI IDC Header,,'}}}]
